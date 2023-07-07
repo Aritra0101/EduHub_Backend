d107d0eb65fda124340252147f1ef75fb9017b2b
Aritra Chatterjee @Aritra0101 <aritra_chatterjee@hotmail.com>
2023-07-08 00:56:52 +0530

    userModel updated

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -4,12 +4,12 @@ const router = express.Router();
 import { checkUserName, studentLogin, studentRegister, teacherLogin, teacherRegister } from "../controllers/userController.js";
 
 
-router.post('/student/login', studentLogin);
-router.post('/student/register', studentRegister);
-router.post('/teacher/login', teacherLogin);
-router.post('/teacher/register', teacherRegister);
+router.post('/user/student/login', studentLogin);
+router.post("/user/student/register", studentRegister);
+router.post("/user/teacher/login", teacherLogin);
+router.post("/user/teacher/register", teacherRegister);
 
-router.post('/checkUserName', checkUserName);
+router.post("/user/checkUserName", checkUserName);
 
 
 
